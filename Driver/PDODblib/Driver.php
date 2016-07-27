2fc7f72bcb55e955bb4211701ffa0c1caf9a42ce
stevro <stevro@users.noreply.github.com>
2016-07-27 15:26:41 +0300

    Update Driver.php
    fixed DSN according to PHP docs

--- Driver/PDODblib/Driver.php
+++ Driver/PDODblib/Driver.php
@@ -59,11 +59,13 @@ class Driver implements \Doctrine\DBAL\Driver
             $dsn = 'sqlsrv:server=';
 
             if (isset($params['host'])) {
-                $dsn .= $params['host'];
-            }
+                $dsn .= 'host=' . $params['host'];
 
-            if (isset($params['port']) && !empty($params['port'])) {
-                $dsn .= ',' . $params['port'];
+                if (isset($params['port'])) {
+                    $dsn .= ':' . $params['port'];
+                }
+
+                $dsn .= ';';
             }
 
             if (isset($params['dbname'])) {
